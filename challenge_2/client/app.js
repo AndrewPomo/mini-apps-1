80200ecba6b1ce922cdaba3c21143e8aedd6c2c2
Andrew Powell-Morse <andrewpomo815@gmail.com>
2018-04-03 12:34:24 -0700

    Complete submission of json

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,19 @@
+$( document ).ready(function() {
+  
+  $('#submit').on('click', function() {
+    handleSubmit($('#JSON').val());
+  })
 
+  var handleSubmit = function(json) {
+    $.ajax({
+      type: "POST",
+      url:"http://127.0.0.1:3000/",
+      contentType: "application/json",
+      data: json,
+      success:function(data) {
+        console.log(data)
+      }
+    });
+  }
 
+});
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,48 @@
+var express = require('express')
+var path = require('path');
+var bodyParser = require('body-parser');
+var app = express();
+
+var handleJSON = function(JSON, callback) {
+  var keys = Object.keys(JSON);
+  // keys = the first line of the csv
+
+  function getVals() {
+
+    for (var keys in JSON) {
+
+    }
+  }
+
+  getVals(JSON)
+  
+  callback(JSON)
+}
+
+app.use(express.static(__dirname + '/client'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './client', 'index.html'))
+});
+
+app.post('/', function (req, res) {
+  handleJSON(req.body, function(csv) {
+    res.send(csv)
+  })
+})
+
+app.listen(3000, () => console.log('Listening on port 3000!'))
 
 
+// You may assume the JSON data has a regular structure and hierarchy (see included sample file). 
+
+// The server must flatten the JSON hierarchy, mapping each item in the JSON to a single line of CSV report (see included sample output). 
+
+// You may assume child records in the JSON will always be in a property called children but you may not assume a JSON record has any other specific properties; 
+// i.e. any properties that exist besides children must be mapped to a column in your CSV report.
+
+// var CSVify = function (stringiFied) {
+
+// }
